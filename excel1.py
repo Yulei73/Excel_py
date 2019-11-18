5ffbeb28312b3bdcb942c5c0b79d87fb850accfc
Yulei73 <48815636+Yulei73@users.noreply.github.com>
2019-11-18 13:17:21 +0800

    Add files via upload

---
+++ excel1.py
@@ -0,0 +1,60 @@
+import xlrd   #读取
+import xlwt   #生成
+
+num8 = [15211206,
+       16221222,
+       16221071,
+       16231234,
+       16231230,
+       16231206,
+       16211411,
+       16211405,
+       16211403,
+       16211389,
+       16211409,
+       16211402,
+       16211399,
+       16211368,
+       16211366,
+       16211378,
+       16211377,
+       16211379,
+       16211373,
+       16211380,
+       16221013,
+       16211375,
+       16211369,
+       16211393,
+       16211372,
+       16211412,
+       16221241,
+       16211406,
+       16211388,
+       16211401,
+       16231261]
+
+print(num8[1])
+workbook = xlwt.Workbook()
+sheet1 = workbook.add_sheet('sheet1', cell_overwrite_ok=True)
+
+for i in range(31):
+    #tworkbook = xlrd.open_workbook(u'pytest.xlsx')
+    name = str(num8[i]) + '.xlsx'
+    try:
+        tworkbook = xlrd.open_workbook(str(name))
+        tsheet = tworkbook.sheet_by_name('Sheet1')
+        rows = 3  # 获取行数
+        cols = 20  # 获取列数
+        for j in range(cols):  # 读取每一列的数据
+            #print(tsheet.cell(3,j))
+            con1 = tsheet.cell(3, j)
+            con2 = str(con1).strip('\'').split(':')
+            print(con2)
+            sheet1.write(i, j, con2[1].strip('.0').strip('\''))
+
+        #sheet1.write(i, 0, namestr[0])
+    except Exception as e:
+        print(e)
+
+
+workbook.save('all.xls')
